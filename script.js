46bfd30a2958a30ff8a2204da46099303536e482
AnyElizabeth29 <125214292+AnyElizabeth29@users.noreply.github.com>
2023-11-08 11:36:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function soma_seq() {
+
+    var n=parseInt(numero.value);
+    var soma=n;
+
+
+    while (n>1) {
+        soma=soma+(n-1)
+        n=n-1;
+
+    }
+
+    alert(soma);
+    
+}
+function
